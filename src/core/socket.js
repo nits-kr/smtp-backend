c6b73d5eb5267ea08f24e8197148ad5432ea0089
Nitish Kumar <nits.kr99@gmail.com>
2026-01-04 20:08:41 +0530

    socket id

--- src/core/socket.js
+++ src/core/socket.js
@@ -1,35 +1,36 @@
-const logger = require('./logger');
+const logger = require("./logger");
 let io = null;
 
 const initSocket = (server) => {
-    const { Server } = require('socket.io');
-    io = new Server(server, {
-        cors: {
-            origin: '*', // Allow all for dev; restrict in production
-            methods: ['GET', 'POST']
-        }
+  const { Server } = require("socket.io");
+  io = new Server(server, {
+    cors: {
+      origin: "*", // Allow all for dev; restrict in production
+      //   origin: "http://localhost:5173",
+      methods: ["GET", "POST"],
+    },
+  });
+  logger.info("Socket.io initialized");
+
+  io.on("connection", (socket) => {
+    logger.info(`Socket connected: ${socket.id}`);
+
+    socket.on("disconnect", () => {
+      logger.info(`Socket disconnected: ${socket.id}`);
     });
-    logger.info('Socket.io initialized');
+  });
 
-    io.on('connection', (socket) => {
-        logger.info(`Socket connected: ${socket.id}`);
-
-        socket.on('disconnect', () => {
-            logger.info(`Socket disconnected: ${socket.id}`);
-        });
-    });
-
-    return io;
+  return io;
 };
 
 const getIo = () => {
-    if (!io) {
-        throw new Error('Socket.io not initialized!');
-    }
-    return io;
+  if (!io) {
+    throw new Error("Socket.io not initialized!");
+  }
+  return io;
 };
 
 module.exports = {
-    initSocket,
-    getIo
+  initSocket,
+  getIo,
 };
--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,24 +1,25 @@
 let io = null;
 
 const initSocket = (server) => {
-    const { Server } = require('socket.io');
-    io = new Server(server, {
-        cors: {
-            origin: '*', // Allow all for dev; restrict in production
-            methods: ['GET', 'POST']
-        }
-    });
-    return io;
+  const { Server } = require("socket.io");
+  io = new Server(server, {
+    cors: {
+      origin: "*",
+      //   origin: "http://localhost:5173",
+      methods: ["GET", "POST"],
+    },
+  });
+  return io;
 };
 
 const getIo = () => {
-    if (!io) {
-        throw new Error('Socket.io not initialized!');
-    }
-    return io;
+  if (!io) {
+    throw new Error("Socket.io not initialized!");
+  }
+  return io;
 };
 
 module.exports = {
-    initSocket,
-    getIo
+  initSocket,
+  getIo,
 };
